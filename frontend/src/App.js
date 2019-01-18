08b9a1414a496e93272643d5e094a276598cb63b
Kahvi Patel <iamkahvi@gmail.com>
2019-01-18 09:30:07 -0600

    works now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,19 +7,19 @@ import {
   Switch
 } from 'react-router-dom';
 
-
 import Header from './components/header/header.js';
 import Homepage from './components/pages/homepage.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <Header />
-          <Switch>
-            <Route exact path='/' component={Homepage} />
-          </Switch>
-      </div>
+      <Router>
+        <div className="App">
+            <Header />
+            <Switch>
+            </Switch>
+        </div>
+      </Router>
     );
   }
 }
