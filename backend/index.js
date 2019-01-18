1f0cdc7ca70575e79598ec8f6ee3bfbdc4ccb7be
Jayden Chan <jaydencn7@gmail.com>
2019-01-18 08:51:24 -0600

    Board getter and setters

--- backend/index.js
+++ backend/index.js
@@ -20,10 +20,29 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+let board;
+
 app.use(bodyParser.json());
 
 app.get('/', (req, res) =>  {
   res.send('Hello World!');
+});
+
+app.get('/new' (req, res) => {
+  if (req.body.size === null) {
+    res.code(400).send('need board size');
+  }
+
+  board = Board(req.body.size);
+  res.code(200).send(JSON.stringify(board));
+});
+
+app.get('/board' (req, res) => {
+  if (board instanceof Board) {
+    res.code(200).send(JSON.stringify(board));
+  } else {
+    res.status(400).send('Board not created yet');
+  }
 });
 
 app.listen(port, () => console.log(`WEC 2019 server app Listening on port ${port}!`));
