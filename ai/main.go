459af92b3f8a42657d5ba391f4b81fe8ecdcce64
Jayden Chan <jaydencn7@gmail.com>
2019-01-18 13:47:02 -0600

    Updates to main.go by Cobey
    Co-authored-by: Cobey Hollier <cobey.hollier@mac.com>

--- ai/main.go
+++ ai/main.go
@@ -1,7 +1,50 @@
 package main
 
 import "fmt"
+import "os"
+import "net/http"
+import "encoding/json"
 
 func main() {
-	fmt.Println("Hello world!")
+	newGame()
+
+}
+
+func newGame() {
+	var size int
+	fmt.Println("Please Enter a Board Size")
+	_, err := fmt.Scan(&size)
+	if(err != nil) {
+		fmt.Println("Scanning Error. Exiting Program")
+		os.Exit(3)
+	}
+	fmt.Println("The size is", size)
+
+	data := map[string]int{"size": size}
+	marshalled, err := json.Marshal(data)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	resp, err := http.Post("http://localhost:5000/new", string(marshalled), nil)
+	if err != nil {
+		fmt.Println("Game creation request failed")
+		os.Exit(1)
+	}
+	fmt.Println(resp)
+	move(size/2, size/2)
+
+}
+
+
+func move(x int, y int) {
+	data := map[string]int{"x": x, "y": y}
+	fmt.Println(data)
+	marshalled, err := json.Marshal(data)
+	fmt.Println(string(marshalled))
+	if err != nil {
+		os.Exit(1)
+	}
+	resp, err := http.Post("http://localhost:5000/click", string(marshalled), nil)
+	fmt.Println(resp)
 }
